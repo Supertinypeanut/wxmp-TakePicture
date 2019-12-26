826a960e053810976f2b424981431f1969174c67
supertinypeaunt <1176032017@qq.com>
2019-12-26 21:23:15 +0800

    调用百度AI获取图片检测信息

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ App({
   onLaunch: function () {
     // 获取接口token信息
     wx.request({
+      method: 'POST',
       url: 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=4fIgPHXoD8XPNAOALPpm5NOQ&client_secret=y9RdZHWBY8sWR1YrUAQ81TdtzefVwn7P',
-      success:function(res){
+      success:(res) => {
         const { data } = res
         // 更新token信息
-        this.tokenMessage = {
+        this.globalData.tokenMessage = {
           access_token: data.access_token,
           refresh_token: data.refresh_token
         }
-      }.bind(this)
+      }
     })
   },
   globalData: {
--- pages/takePicture/index.js
+++ pages/takePicture/index.js
@@ -1,6 +1,9 @@
 // pages/takePicture/index.js
-Page({
 
+// 在页面中，可以调用 getApp() 函数，获取到全局的一些数据
+const app = getApp()
+
+Page({
   /**
    * 页面的初始数据
    */
@@ -8,8 +11,86 @@ Page({
     // 摄像头方向
     devicePosition: "front",
     // 拍照生成的图片路径
-    tempImagePath: ''
+    tempImagePath: '',
+    // 图片检测信息
+    faceInfo: null
   },
+  
+  // 改变摄像头方向
+  onChangeDevicePosition(){
+    console.log(this.data)
+    this.setData({
+      devicePosition: this.data.devicePosition === "front" ? "back" : "front"
+    })
+  },
+
+  // 拍照
+  onTakePicture(){
+    // 创建一个camera内容对象
+    const ctxt = wx.createCameraContext()
+    // 调用拍照
+    ctxt.takePhoto({
+      quality: 'high',
+      success:(res) => {
+
+        // 检验是否拍照成功
+        if( res.errMsg !== "operateCamera:ok"){
+          return wx.showToast({
+            title: '拍照失败！',
+            icon: 'none'
+          })
+        }
+
+        // 更新图片路径
+        this.setData({
+          // 存储图片路径
+          tempImagePath : res.tempImagePath
+        },() => {
+          // 调用检测人脸信息函数
+          this.getFaceInfo()
+        })
+      }
+    })
+  },
+
+  // 获取图片检测信息
+  getFaceInfo(){
+    wx.showLoading({
+      title: '颜值检测中...'
+    })
+
+    // 获取全局token
+    const token = app.globalData.tokenMessage.access_token
+
+    // 获取base64的图片
+    const fileManager = wx.getFileSystemManager()
+    const base64Img = fileManager.readFileSync(this.data.tempImagePath, 'base64')
+
+    // 调用百度API检测图片
+    wx.request({
+      method: 'POST',
+      url: 'https://aip.baidubce.com/rest/2.0/face/v3/detect?access_token='+ token,
+      header:{
+        'Content-Type': 'application/json'
+      },
+      data:{
+        image_type:'BASE64',
+        image: base64Img,
+        face_field: 'age,beauty,expression,face_shape,gender,glasses,landmark,landmark150,race,quality,eye_status,emotion,face_type'
+      },
+      success:function(res){
+        // 更新图片检测信息
+        wx.setData({
+          faceInfo: res.data.result.face_list[0]
+        })
+      },
+      complete:function(){
+        // 关闭loading
+        wx.hideLoading()
+      }
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
@@ -66,42 +147,5 @@ Page({
   onShareAppMessage: function () {
 
   },
-  
-  // 改变摄像头方向
-  onChangeDevicePosition(){
-    console.log(this.data)
-    this.setData({
-      devicePosition: this.data.devicePosition === "front" ? "back" : "front"
-    })
-  },
-
-  // 拍照
-  onTakePicture(){
-    // 创建一个camera内容对象
-    const ctxt = wx.createCameraContext()
-    // 调用拍照
-    ctxt.takePhoto({
-      quality: 'high',
-      success:function(res){
-
-        // 检验是否拍照成功
-        if( res.errMsg !== "operateCamera:ok"){
-          return wx.showToast({
-            title: '拍照失败！',
-            icon: 'none'
-          })
-        }
-
-        // 更新处理图片路径
-        this.setData({
-          tempImagePath : res.tempImagePath
-        })
-
-        console.log(88)
-      }
-    })
-
-
-  }
 
 })
